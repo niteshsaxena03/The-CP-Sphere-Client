135a1d00ccf3b3d9f6e300208c28b9c5c3b7a99b
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-10-16 18:54:19 +0530

    Adding mobile optimization to rank card

--- src/components/RankCard.jsx
+++ src/components/RankCard.jsx
@@ -1,25 +1,27 @@
-// components/RankCard.js
 const RankCard = ({ ranking, username, rating }) => {
   // Construct the URL for the LeetCode profile
   const leetcodeProfileUrl = `https://leetcode.com/u/${username}/`;
 
-  
   return (
     <a
       href={leetcodeProfileUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="bg-gradient-to-r from-red-800 to-blue-800 text-white rounded-lg p-6 m-4 shadow-lg hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105 flex justify-between items-center border border-red-700"
+      className="bg-gradient-to-r from-red-800 to-blue-800 text-white rounded-lg p-4 sm:p-6 m-2 sm:m-4 shadow-lg hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105 flex justify-between items-center border border-red-700"
     >
       {/* Left side - Ranking and Username */}
       <div>
-        <h2 className="text-5xl font-extrabold text-yellow-300">#{ranking}</h2>
-        <h3 className="text-2xl font-semibold mt-2">{username}</h3>
+        <h2 className="text-3xl sm:text-5xl font-extrabold text-yellow-300">
+          #{ranking}
+        </h2>
+        <h3 className="text-lg sm:text-2xl font-semibold mt-2">{username}</h3>
       </div>
 
       {/* Right side - Rating */}
       <div className="text-right">
-        <p className="text-3xl font-bold text-green-300">Rating: {rating}</p>
+        <p className="text-xl sm:text-3xl font-bold text-green-300">
+          Rating: {rating}
+        </p>
       </div>
     </a>
   );
