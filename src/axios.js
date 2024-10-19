1c6eb5510e513bdc07b17c9b6fbaa09481f8d9a9
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-10-19 09:01:21 +0530

    Changed backend api

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Define the base URL for all requests
+  baseURL: "https://the-cp-sphere-server-1.onrender.com", // Define the base URL for all requests
 });
 
 export default api;
